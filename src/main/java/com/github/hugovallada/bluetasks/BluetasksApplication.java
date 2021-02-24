ae08f4c139d87d5aaeb4b9c7a5c10b82f76ca255
hugovallada <valladahugo@gmail.com>
2021-02-24 19:41:40 -0300

    Adiciona cors

--- src/main/java/com/github/hugovallada/bluetasks/BluetasksApplication.java
+++ src/main/java/com/github/hugovallada/bluetasks/BluetasksApplication.java
@@ -2,6 +2,8 @@ package com.github.hugovallada.bluetasks;
 
 import com.github.hugovallada.bluetasks.domain.task.Task;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +12,26 @@ import org.springframework.data.rest.core.event.ValidatingRepositoryEventListene
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class BluetasksApplication implements RepositoryRestConfigurer {
 
+	private static final Logger logger = LoggerFactory.getLogger(BluetasksApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BluetasksApplication.class, args);
+		logger.info("Bluetasks is ready for work");
 	}
 
 	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Task.class);
+
+		// CORS
+		cors.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+
+		logger.info("Repository CORS setup... OK");
 	}
 
 	@Bean
@@ -33,6 +44,8 @@ public class BluetasksApplication implements RepositoryRestConfigurer {
 		var validator = validator();
 		vrel.addValidator("beforeCreate", validator);
 		vrel.addValidator("beforeSave", validator);
+
+		logger.info("Configure validator... OK!");
 	}
 
 }
